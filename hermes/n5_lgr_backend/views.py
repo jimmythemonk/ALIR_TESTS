2b4e32cd99c24216c084ed114176312a95e123a2
James Foley <jamesfoley@alps.ie>
2024-04-11 10:22:42 +0100

    New changes

--- hermes/n5_lgr_backend/views.py
+++ hermes/n5_lgr_backend/views.py
@@ -17,6 +17,16 @@ def backend(request):
             device_serial=current_serial
         ).order_by("-create_at")
 
+    # Check for message gaps
+    prev_seq_num = None
+    for data in message_data:
+        data.is_incremental = prev_seq_num is None or data.seq_num == prev_seq_num + 1
+        prev_seq_num = data.seq_num
+
+    # Most recent message item is always incremental
+    if message_data:
+        message_data[0].is_incremental = False
+
     context = {
         "message_data": message_data,
         "serials": serials,
