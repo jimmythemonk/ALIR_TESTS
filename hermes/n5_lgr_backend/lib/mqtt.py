d1254cd7db4e7ae76bb21276480222a4cdcd1551
James Foley <jamesfoley@alps.ie>
2024-04-23 10:15:15 +0100

    Remove print statements

--- hermes/n5_lgr_backend/lib/mqtt.py
+++ hermes/n5_lgr_backend/lib/mqtt.py
@@ -17,7 +17,6 @@ def on_message(mqtt_client, userdata, msg):
 
     from ..models import TestDevice, TestSerialData
 
-    print(f"Message In")
     start_time = time.time()
 
     message = msg.payload.decode()
@@ -75,9 +74,6 @@ def on_message(mqtt_client, userdata, msg):
         item.save()
 
     total_time = time.time() - start_time
-    print(
-        f"Message parsed ({device_id}-{seq_num}) and saved to database: {total_time} seconds"
-    )
 
 
 client = mqtt.Client(
